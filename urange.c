ef09730881d7e4f9708f4580f5bf4dbad2ab9de8
Tearzo <pornsakul@gmail.com>
2023-09-09 12:43:11 +0200

    ultrng

---
+++ urange.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+
+int	ft_ultimate_range(int **range, int min, int max)
+{
+	int	i;
+
+	if (min >= max)
+	{
+		*range = (NULL);
+		return (0);
+	}
+	*range = malloc((max - min) * sizeof(int));
+	if (*range == 0)
+		return (-1);
+	i = 0;
+	while (i < max - min)
+	{
+		(*range)[i] = min + i;
+		i++;
+	}
+	return (i);
+}
+
+#include <stdio.h>
+int	main(void)
+{
+	int *range;
+
+	printf("Warn: Any segfault will mean that arrays are not correctly allocated\n");
+
+	printf("5:%d\n", ft_ultimate_range(&range, 0, 5));
+	printf("0, 1, 2, 3, 4 : %d, %d, %d, %d, %d\n", 
+	range[0], range[1], range[2], range[3], range[4]);
+
+	printf("0:%d\n", ft_ultimate_range(&range, 3, 3));
+
+	printf("2:%d\n", ft_ultimate_range(&range, -1, 1));
+	printf("-1, 0 : %d, %d", range[0], range[1]);
+}
